c93e4877d46a42eac49dfb445eb48d82c25896aa
boke1208 <boke1208@gmail.com>
2018-04-07 00:17:01 +0800

    写了一点

---
+++ DiskOperate.cpp
@@ -0,0 +1,16 @@
+
+typedef class DiskOperate{
+public:
+	bool write(int startBlock, string content);//从哪开始写，写啥
+	string read(int startBlock, int dataSize);//从哪，多长
+}DiskOperate;
+
+bool DiskOperate::write(int startBlock, string content){
+
+}
+
+string DiskOperate::read(int startBlock, int dataSize){
+
+
+	
+}
--- FileSystem.cpp
+++ FileSystem.cpp
@@ -1,13 +1,21 @@
 #include<bits/stdc++.h>
+#include "DiskOperate.cpp"
+#define system_size 100*1024*1024   //系统大小  
+#define block_szie 1024 //盘块大小  
+#define block_count system_size/block_szie //系统盘块数目
 
 using namespace std;
+
+char* systemStartAddr
+string currentDir;
+DirOperate dirOp;
 
 //磁盘操作
 typedef class DiskOperate{
 public:
 	bool write(int startBlock, string content);//从哪开始写，写啥
 	bool read(int startBlock, int dataSize);//从哪，多长
-}
+}DiskOperate;
 
 //指令层
 //推荐在这里负责目录逻辑，仅返回status，在命令行里根据返回status，决定此次操作正确与否
@@ -22,7 +30,7 @@ public:
 	void list_directory();
 	void touch_directory();//创建目录
 	void rm_directory();
-}
+}DirOperate;
 
 
 //复制了word
@@ -55,17 +63,46 @@ void example(){
 	string op;
 	//这里是命令行，在这里进行初始化，循环，交互
 	while (1){
-		cout << "[root@localhost ~]";
+		cout << "[root@localhost " + currentDir + "]";
 		cin >> op;
-		switch (op){
-			//应该有成形的命令行解析头文件
+		bool endThisOperate = false;
+		fflush(stdin);
+		string[] args = op.split(' ');
+		//这里加多空格容错
+		switch (args[0]){
+			case "ls":
+			case "ll":
+				//列出目录
+				dirOp.list_directory("");
+				endThisOperate = true;
+				break;
+			case "touch":
+				dirName = args[1];
+				//检查是否存在此目录，不存在则创建
+				break;
+
+			case "cat":
+				fileName = args[1];
+				//查看文件信息
+				break;
+			case ""
+
+
 		}
 
+		
 	}
 }
 
 void init_system(){
-	//读入存储在
+	systemStartAddr = (char*)malloc(system_size * sizeof(char));  
+    //初始化盘块的位示图  
+    for(int i=0; i<block_count; i++)  
+        systemStartAddr[i] = '0';  
+    //用于存放位示图的空间已被占用  
+    int bitMapSize = block_count * sizeof(char) / block_szie;//位示图占用盘块数:100  
+    for(int i=0; i<bitMapSize; i++)//从零开始分配  
+        systemStartAddr[i] = '1';   //盘块已被使用  
 }
 
 void test_unit(){
