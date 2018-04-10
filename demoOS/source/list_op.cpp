227735391617280d39880c3e18b4b93924088777
Biuuuuuuuu <biuuuuuuuu@hotmail.com>
2018-04-10 23:30:22 +0800

    进程队列基础设施。下一步：进程调度。

--- demoOS/source/list_op.cpp
+++ demoOS/source/list_op.cpp
@@ -1,62 +1,91 @@
 #include "list_op.h"
 
-//新建一个进程还需要其他信息吧
-void newProcess(QList<Process>& pcbPool, QList<Process*> & list,unsigned long lastPID)
+//获取新PID,返回0表示失败
+unsigned long getNewPID(QList<Process*>& pcbPool)
 {
-    while(find(pcbPool,lastPID+1)){
-        lastPID=lastPID+1;
+    if (pcbPool.isEmpty()) return 1;
+    unsigned long max=0;
+    for(int i=0;i<pcbPool.size();i++){
+        unsigned long thisPID=pcbPool.at(i)->getPid();
+        if(thisPID>max) max=thisPID;
     }
-    Process* newP=new Process(lastPID+1);
-    pcbPool.append(*newP);
-    list.append(newP);
+    max=max+1;
+
+    if(max!=0) return max;
+
+    //max上溢到0,pid可能满了
+    max=1;
+    while (find(pcbPool,max)!=nullptr && max!=0)
+        max++;
+    if(max==0) return 0;//所有pid都被占用了。
+    else return max;
+}
+
+//新建一个进程
+Process* newProcess(QList<Process*>& pcbPool)
+{
+    unsigned long newPID=getNewPID(pcbPool);
+    if(newPID==0) return nullptr;//没有可用PID 失败
+
+    Process* newP=new Process(newPID);
+    pcbPool.append(newP);
+    return newP;
 }
 
 //状态转移 s_list -> d_list
 void chgState(QList<Process*>& s_list, QList<Process*>&d_list, unsigned long PID){
-    for ( int i=0; i!=s_list.size(); ++i )
-    {
-       if ( s_list.at(i)->getPid() == PID)
-       {
-           d_list.append(s_list.at(i));
+    Process* p=nullptr;
+    for ( int i=0; i!=s_list.size(); ++i ){//在s_list中查找PID
+       Process* pcbi=s_list.at(i);
+       if (pcbi->getPid() == PID){//找到
+           p = pcbi;
            s_list.removeAt(i);
-            //删除元素这有问题##############
+           break;
        }
     }
+    if(p==nullptr) return;//s_list中没有这个进程，失败。
+    //假设进程在d_list不存在
+    d_list.append(p);
+    return;
 }
 
-void termiProcess(QList<Process> &pcbPool, QList<Process*> &list, unsigned long PID)
+void termiProcess(QList<QList<Process*>> &lists, unsigned long PID)
 {
-    for ( int i=0; i!=list.size(); ++i )
-    {
-       if ( list.at(i)->getPid() == PID)
-       {   delete(list.at(i));//指针的释放可能有问题
-           list.removeAt(i);
-       }
+    Process* p=nullptr;
+    for(int i=0;i<lists.size();i++){
+        QList<Process*> list=lists.at(i);
+        p=find(list,PID);
+        if(p!=nullptr)break;//找到进程指针
     }
-
-    for ( int i=0; i!=pcbPool.size(); ++i )
-    {
-       if ( pcbPool.at(i).getPid() == PID)
-       {
-           pcbPool.removeAt(i);
-       }
+    if(p==nullptr)return;//进程不存在
+    for(int i=0;i<lists.size();i++){//从所有队列删除
+        QList<Process*> list=lists.at(i);
+        for(int j=0;j<list.size();j++)
+            if(list.at(j)->getPid()==PID){//删除队列中进程,假设只有一个
+                list.removeAt(j);
+                break;
+            }
     }
+    delete p;//释放
+    return;
 }
 
-bool find(QList<Process> pcbPool, unsigned long PID){
-    for ( int i=0; i!=pcbPool.size(); ++i )
-    {
-       if ( pcbPool.at(i).getPid() == PID)
-       {
-           return true;
+Process* find(QList<Process*> list, unsigned long PID)
+{
+    for ( int i=0; i!=list.size(); ++i ){
+       Process* pcbi=list.at(i);
+       if ( pcbi->getPid() == PID){
+           return pcbi;
        }
     }
-    return false;
+    return nullptr;
 }
 
-void printQue(QList<Process*> q)
-{   for ( int i=0; i!=q.size(); ++i )
-    {
-       qDebug()<<QString::number(q.at(i)->getPid());
+QString printQue(QList<Process*> &q)
+{
+    QString ret;
+    for ( int i=0; i!=q.size(); ++i ){
+       ret+=QString::number(q.at(i)->getPid())+" ";
     }
+    return ret;
 }
--- demoOS/source/list_op.h
+++ demoOS/source/list_op.h
@@ -2,19 +2,19 @@
 #define LIST_OP_H
 #include "mainwindow.h"
 
-//pcb池、new队列添加进程
-void newProcess(QList<Process>& pcbpool, QList<Process*> & list,unsigned long lastPID);
+//创建新进程(空的),并存放在pcbPool中,若失败返回nullptr
+Process* newProcess(QList<Process*> &pcbPool);
 
-//状态转移 s_list -> d_list
-void chgState(QList<Process*>& s_list, QList<Process*>& d_list, unsigned long PID);
+//在队列间移动 s_list -> d_list
+void move(QList<Process*>& s_list, QList<Process*>& d_list, unsigned long PID);
 
-//结束进程，所处队列和pcb池中删除进程
-void termiProcess(QList<Process>& pcbPool, QList<Process*>& list,unsigned long PID);
+//结束进程，在所有list中删除进程,然后释放Process对象
+void termiProcess(QList<QList<Process*>> &lists, unsigned long PID);
 
-//在pcb池中是否存在进程Pid
-bool find(QList<Process> pcbpool, unsigned long PID);
+//list中查找PID,若没有返回nullptr
+Process* find(QList<Process*> list, unsigned long PID);
 
-//查看队列内容
-void printQue(QList<Process*> q);
+//队列状态打印
+QString printQue(QList<Process*> q);
 
 #endif // LIST_OP_H
